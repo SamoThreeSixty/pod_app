972f548030c28105042b8b8b80400247c4f38043
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-09-23 22:05:41 +0100

    Auth BLoC events

--- lib/features/auth/presentation/bloc/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -2,3 +2,15 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+
+final class AuthSignIn extends AuthEvent {
+  final String email;
+  final String password;
+
+  AuthSignIn(
+    this.email,
+    this.password,
+  );
+}
+
+final class AuthSignOut extends AuthEvent {}
