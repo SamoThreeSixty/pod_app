8d4e93a59e1ea9f740ec8857adba137c9aa768e2
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-09-23 23:08:30 +0100

    Added SupabaseException for more detailed response

--- lib/core/event/events.dart
+++ lib/core/event/events.dart
@@ -7,3 +7,15 @@ class ServerException implements Exception {
   final String message;
   const ServerException(this.message);
 }
+
+class SupabaseException implements Exception {
+  final String message;
+  final String code;
+  final int statusCode;
+
+  const SupabaseException(
+    this.message,
+    this.code,
+    this.statusCode,
+  );
+}
