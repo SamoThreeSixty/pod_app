253805ffb798b41e283f30a50353a049c799a6b7
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-09-23 21:02:11 +0100

    Added Supabase to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'dart:developer';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:pod_app/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  debugger();
+
+  await dotenv.load(fileName: '.env');
+
+  final url = dotenv.env['SUPABASE_URL'];
+  final anonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+  if (url == null || anonKey == null) {
+    throw Exception('Supabase environment variables are not loaded properly.');
+  }
+
+  await Supabase.initialize(
+    url: url,
+    anonKey: anonKey,
+  );
+
   runApp(MyApp());
 }
 
@@ -12,6 +34,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'PoD Application',
       debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
