8dc1d61228605c7b55aba9b9ecde891fc0075419
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-09-23 21:05:16 +0100

    Setup Auth Widgets - Presentation Layer

---
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AuthField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscureText;
+  final Icon icon;
+
+  const AuthField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        prefixIcon: icon,
+        hintText: hintText,
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "$hintText is missing";
+        }
+
+        return null;
+      },
+      obscureText: isObscureText,
+    );
+  }
+}
