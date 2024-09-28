7df8048b271c288a13e01bd866edcca46a0317d7
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-09-28 23:17:05 +0100

    DeliveryDetail entity

---
+++ lib/features/delivery_record/domain/entity/delivery_detail.dart
@@ -0,0 +1,27 @@
+class DeliveryDetail {
+  final int id;
+  final int delivery_header_id;
+  final DateTime created_at;
+  final int status;
+  final String stock_ref;
+  final int quantity;
+  final String unit;
+  final bool loaded;
+  final DateTime loaded_date;
+  final bool delivered;
+  final DateTime delivered_date;
+
+  DeliveryDetail({
+    required this.id,
+    required this.delivery_header_id,
+    required this.created_at,
+    required this.status,
+    required this.stock_ref,
+    required this.quantity,
+    required this.unit,
+    required this.loaded,
+    required this.loaded_date,
+    required this.delivered,
+    required this.delivered_date,
+  });
+}
