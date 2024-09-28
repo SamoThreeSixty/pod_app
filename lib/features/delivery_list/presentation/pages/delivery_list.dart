4fd8be6be44813bc4ba90e3b3145030e4db461a5
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-09-28 23:22:04 +0100

    DeliveryList shows side by side widgets

--- lib/features/delivery_list/presentation/pages/delivery_list.dart
+++ lib/features/delivery_list/presentation/pages/delivery_list.dart
@@ -2,7 +2,9 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pod_app/features/delivery_list/domain/entity/delivery_header.dart';
 import 'package:pod_app/features/delivery_list/presentation/bloc/delivery_list_bloc.dart';
+import 'package:pod_app/features/delivery_list/presentation/widgets/delivery_record.dart';
 
 class DeliveryList extends StatefulWidget {
   const DeliveryList({super.key});
@@ -22,16 +24,49 @@ class _DeliveryListState extends State<DeliveryList> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DeliveryListBloc, DeliveryListState>(
-      builder: (context, state) {
-        debugger();
-        if (state is DeliveryListSuccess) {
-          return const Text("success");
-        } else if (state is DeliveryListFailure) {
-          return const Text("big fails");
-        }
-        return Container();
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Delivery List'),
+      ),
+      body: BlocBuilder<DeliveryListBloc, DeliveryListState>(
+        builder: (context, state) {
+          if (state is DeliveryListSuccess) {
+            debugger();
+            // Going to get a array containing a list of 2 deliveries
+            final deliveryPairs = <List<DeliveryHeader>>[];
+
+            for (var i = 0; i < state.deliveryHeader.length; i += 2) {
+              // Stepping in two so we can display side by side
+              deliveryPairs.add(state.deliveryHeader.sublist(
+                  i,
+                  i + 2 > state.deliveryHeader.length
+                      ? state.deliveryHeader.length
+                      : i + 2));
+            }
+
+            return ListView.builder(
+              itemCount: deliveryPairs.length,
+              itemBuilder: (context, index) {
+                final deliveries = deliveryPairs[index];
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: deliveries.map((delivery) {
+                    return Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: DeliveryRecord(delivery: delivery),
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+            );
+          } else if (state is DeliveryListFailure) {
+            return const Text("big fails");
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
