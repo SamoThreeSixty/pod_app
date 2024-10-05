2c9ef10b9bcccee7fa07a5bb5db0d397810ec56d
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-10-05 19:51:39 +0100

    Simplified the entity and model of the event log

--- lib/features/event/data/model/event_log_model.dart
+++ lib/features/event/data/model/event_log_model.dart
@@ -8,34 +8,18 @@ class EventLogModel extends EventLog {
   EventLogModel({
     required super.LogID,
     required super.EventDate,
-    required super.ModuleName,
     required super.Operation,
     required super.Description,
-    required super.ShortDesc,
     required super.UserID,
-    required super.FreeType,
-    required super.SystemEvent,
-    required super.Table,
-    required super.KeyValue,
-    required super.SessionID,
-    required super.System,
   });
 
   factory EventLogModel.fromJson(Map<String, dynamic> json) {
     return EventLogModel(
       LogID: json['LogID'],
       EventDate: json['EventDate'],
-      ModuleName: json['ModuleName'],
       Operation: json['Operation'],
       Description: json['Description'],
-      ShortDesc: json['ShortDesc'],
       UserID: json['UserID'],
-      FreeType: json['FreeType'],
-      SystemEvent: json['SystemEvent'],
-      Table: json['Table'],
-      KeyValue: json['KeyValue'],
-      SessionID: json['SessionID'],
-      System: json['System'],
     );
   }
 
