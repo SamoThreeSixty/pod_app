b2ce46521a5bab86a0313bb6657b2542a8cab7d3
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-10-07 00:09:47 +0100

    Corrected indicator styling

--- lib/core/common/widgets/connectivity_state.dart
+++ lib/core/common/widgets/connectivity_state.dart
@@ -15,54 +15,55 @@ class ConnectivityState extends StatefulWidget {
 class _ConnectivityStateState extends State<ConnectivityState> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => ConnectivityCubit(
-        Connectivity(),
-      ),
-      child: BlocBuilder<ConnectivityCubit, ConnectivityResult>(
-          builder: (context, connectivityStatus) {
-        Color color;
-        IconData icon;
-        String message;
+    return BlocBuilder<ConnectivityCubit, ConnectivityResult>(
+        builder: (context, connectivityStatus) {
+      Color color;
+      IconData icon;
+      String message;
 
-        switch (connectivityStatus) {
-          case ConnectivityResult.none:
-            color = Colors.red;
-            icon = Icons.wifi_off;
-            message = 'You are not connected to the internet.';
-            break;
-          case ConnectivityResult.wifi || ConnectivityResult.mobile:
-            color = Colors.green;
-            icon = Icons.wifi;
-            message = 'You are connected to the internet.';
-            break;
-          default:
-            color = Colors.amber;
-            icon = Icons.abc_sharp;
-            message = 'Unsure of this connection.';
-            break;
-        }
+      switch (connectivityStatus) {
+        case ConnectivityResult.none:
+          color = Colors.red;
+          icon = Icons.wifi_off;
+          message = 'You are not connected to the internet.';
+          break;
+        case ConnectivityResult.mobile:
+        case ConnectivityResult.wifi:
+          color = Colors.green;
+          icon = Icons.wifi;
+          message = 'You are connected to the internet.';
+          break;
+        default:
+          color = Colors.amber;
+          icon = Icons.abc_sharp;
+          message = 'Unsure of this connection.';
+          break;
+      }
 
-        return GestureDetector(
-          onTap: () => _showDetailsDialog(context, message),
-          child: Container(
-            width: 30,
-            height: 30,
-            decoration: BoxDecoration(
-              color: color,
-              shape: BoxShape.circle,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 5.0,
-                  offset: Offset(0, 5), // Shadow position
-                ),
-              ],
-            ),
+      return GestureDetector(
+        onTap: () => _showDetailsDialog(context, message),
+        child: Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 5.0,
+                offset: Offset(0, 5), // Shadow position
+              ),
+            ],
           ),
-        );
-      }),
-    );
+          child: Icon(
+            icon,
+            size: 20,
+            color: Colors.white,
+          ),
+        ),
+      );
+    });
   }
 
   void _showDetailsDialog(BuildContext context, String title) {
