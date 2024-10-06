63769d99ed1274f803a15b217cc7eb155ddbfcb3
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-10-07 00:30:51 +0100

    Save to local db as well

--- lib/features/delivery_record/data/repository/delivery_detail_repository_impl.dart
+++ lib/features/delivery_record/data/repository/delivery_detail_repository_impl.dart
@@ -4,6 +4,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:fpdart/src/either.dart';
 import 'package:pod_app/core/common/cubits/connectivity/connectivity_cubit.dart';
 import 'package:pod_app/core/event/events.dart';
+import 'package:pod_app/features/cached/data/database/cached_image_database.dart';
+import 'package:pod_app/features/cached/data/database/cached_image_dto.dart';
 import 'package:pod_app/features/delivery_record/data/datasource/delivery_detail_local_data_source.dart';
 import 'package:pod_app/features/delivery_record/data/datasource/delivery_detail_remote_data_source.dart';
 import 'package:pod_app/features/delivery_record/domain/entity/delivery_detail.dart';
@@ -72,6 +74,18 @@ class DeliveryDetailRepositoryImpl implements DeliveryDetailRepository {
       if (connectivityStatus == ConnectivityResult.none) {
         // Just save to local
         await deliveryDetailLocalDateSourceImp.saveSignature(path, deliveryID);
+
+        // Save the cache record
+        final cachedImageDto = CachedImageDto(
+          name: 'order_$deliveryID',
+          localPath: path,
+          createdAt: DateTime.now(),
+          syncAction: 'create',
+          isSynced: false,
+          syncedAt: null,
+        );
+
+        CachedImageDatabase.instance.insertCachedImage(cachedImageDto);
       } else {
         // Save to external and local
         await deliveryDetailLocalDateSourceImp.saveSignature(path, deliveryID);
