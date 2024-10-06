0b4016c95226d6f5d4b8e88cdce778105eb4d54b
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-10-06 01:04:34 +0100

    Added separate function for saving signatures to supabase

--- lib/core/service/supabase_storage.dart
+++ lib/core/service/supabase_storage.dart
@@ -11,11 +11,17 @@ abstract interface class ISupabaseStorage {
   /// Returns a [Uint8List] of the image data, or `null` if an error occurs.
   Future<Uint8List?> getImage(String bucket, String image);
 
-  /// Uploads an image to the specified bucket.
+  /// Uploads an image to the images bucket.
   ///
   /// [image] is the name of the image file to upload.
   /// Returns the URL of the uploaded image as a [String].
   Future<String?> uploadPodImage(File image, int orderNo, int imageNumber);
+
+  /// Uploads an image to the signature bucket.
+  ///
+  /// [image] is the name of the image file to upload.
+  /// Returns the URL of the uploaded image as a [String].
+  Future<String?> uploadPodSignature(File image, int orderNo);
 }
 
 class SupabaseStorageService implements ISupabaseStorage {
@@ -40,6 +46,26 @@ class SupabaseStorageService implements ISupabaseStorage {
       String baseName = 'order_${orderNo.toString()}_$imageNumber';
 
       final String fullPath = await supabase.storage.from('pod_images').upload(
+            baseName,
+            image,
+            fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
+          );
+
+      return fullPath;
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  @override
+  Future<String?> uploadPodSignature(File image, int orderNo) async {
+    try {
+      String baseName = 'order_${orderNo.toString()}';
+
+      final String fullPath = await supabase.storage
+          .from('pod_signature')
+          .upload(
             baseName,
             image,
             fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
