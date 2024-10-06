9e8b10a9fd37d7e03cd1ac77fc79049941324572
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-10-06 21:48:22 +0100

    Add single images in repository

--- lib/features/cached/domain/repository/cached_repository.dart
+++ lib/features/cached/domain/repository/cached_repository.dart
@@ -3,12 +3,12 @@ import 'package:pod_app/core/event/events.dart';
 
 // Entities
 import 'package:pod_app/features/cached/domain/entity/cached_data.dart';
-import 'package:pod_app/features/cached/domain/entity/cached_images.dart';
+import 'package:pod_app/features/cached/domain/entity/cached_image.dart';
 
 abstract interface class CachedRepository {
   Future<Either<Failure, void>> addCachedData(CachedData cachedData);
 
-  Future<Either<Failure, void>> addCachedImages(CachedImages cachedImages);
+  Future<Either<Failure, void>> addCachedImage(CachedImage cachedImage);
 
   // Will just do all of it for now. Will return an int of how many synced
   Future<Either<Failure, int>> syncCachedData();
