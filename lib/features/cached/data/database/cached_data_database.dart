8701a9a7b2a9a63c62c9328cf5f7b7090733d25d
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-10-06 22:01:09 +0100

    Added cached data db

--- lib/features/cached/data/database/cached_data_database.dart
+++ lib/features/cached/data/database/cached_data_database.dart
@@ -1,16 +1,16 @@
-import 'package:pod_app/features/cached/data/database/cached_image_dto.dart';
-import 'package:pod_app/features/cached/domain/entity/cached_image.dart';
+import 'package:pod_app/features/cached/data/database/cached_data_dto.dart';
+import 'package:pod_app/features/cached/domain/entity/cached_data.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class CachedImageDatabase {
+class CachedDataDatabase {
   // Singleton instance
-  static final CachedImageDatabase instance = CachedImageDatabase._init();
+  static final CachedDataDatabase instance = CachedDataDatabase._init();
 
   // Database instance
   Database? _database;
 
-  CachedImageDatabase._init();
+  CachedDataDatabase._init();
 
   // Data types
   final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
@@ -22,7 +22,7 @@ class CachedImageDatabase {
   Future<Database> get database async {
     if (_database != null) return _database!;
 
-    _database = await _initDB('cached_images.db');
+    _database = await _initDB('cached_data.db');
     return _database!;
   }
 
@@ -36,46 +36,45 @@ class CachedImageDatabase {
 
   Future<void> _createDB(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE $tableCachedImages (
-        ${CachedImageFields.id} $idType,
-        ${CachedImageFields.name} $textType,
-        ${CachedImageFields.localPath} $textType,
-        ${CachedImageFields.galleryPath} $textType,
-        ${CachedImageFields.createdAt} $textType,
-        ${CachedImageFields.isSynced} $boolType,
-        ${CachedImageFields.syncedAt} $textType,
-        ${CachedImageFields.syncAction} $textType
+      CREATE TABLE $tableCachedData (
+        ${CachedDataFields.id} $idType,
+        ${CachedDataFields.table} $textType,
+        ${CachedDataFields.data} $textType,
+        ${CachedDataFields.createdAt} $textType,
+        ${CachedDataFields.isSynced} $boolType,
+        ${CachedDataFields.syncedAt} $textType,
+        ${CachedDataFields.syncAction} $textType
       )
     ''');
   }
 
-  Future<int> insertCachedImage(CachedImageDto imageDTO) async {
+  Future<int> insertCachedImage(CachedDataDto dataDTO) async {
     final db = await instance.database;
 
     return await db.insert(
-      'cached_images',
-      imageDTO.toJson(),
+      'cached_data',
+      dataDTO.toJson(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  Future<List<CachedImage>> fetchAllCachedImages() async {
+  Future<List<CachedData>> fetchAllCachedImages() async {
     final db = await database;
 
-    const orderBy = '${CachedImageFields.id} ASC';
+    const orderBy = '${CachedDataFields.id} ASC';
 
     final List<Map<String, dynamic>> result =
-        await db.query('cached_images', orderBy: orderBy);
+        await db.query('cached_data', orderBy: orderBy);
 
-    return result.map((json) => CachedImageDto.fromJson(json)).toList();
+    return result.map((json) => CachedDataDto.fromJson(json)).toList();
   }
 
   Future<void> deleteCachedImage(int id) async {
     final db = await instance.database;
 
     await db.delete(
-      tableCachedImages,
-      where: '${CachedImageFields.id} = ?',
+      tableCachedData,
+      where: '${CachedDataFields.id} = ?',
       whereArgs: [id],
     );
   }
---
+++ lib/features/cached/data/database/cached_data_dto.dart
@@ -0,0 +1,83 @@
+import 'package:pod_app/features/cached/domain/entity/cached_data.dart';
+
+const String tableCachedData = 'cached_data';
+
+class CachedDataFields {
+  static final List<String> values = [
+    id,
+    table,
+    data,
+    createdAt,
+    isSynced,
+    syncedAt,
+    syncAction,
+  ];
+
+  static const String id = '_id';
+  static const String table = 'table';
+  static const String data = 'data';
+  static const String createdAt = 'createdAt';
+  static const String isSynced = 'isSynced';
+  static const String syncedAt = 'syncedAt';
+  static const String syncAction = 'syncAction';
+}
+
+class CachedDataDto {
+  final int? id;
+  final String? table;
+  final String? data;
+  final DateTime? createdAt;
+  final bool? isSynced;
+  final DateTime? syncedAt;
+  final String? syncAction;
+
+  const CachedDataDto({
+    this.id,
+    required this.table,
+    required this.data,
+    required this.createdAt,
+    required this.isSynced,
+    required this.syncedAt,
+    required this.syncAction,
+  });
+
+  CachedDataDto copy({
+    int? id,
+    String? name,
+    String? localPath,
+    String? galleryPath,
+    DateTime? createdAt,
+    bool? isSynced,
+    DateTime? syncedAt,
+    String? syncAction,
+  }) =>
+      CachedDataDto(
+        id: id ?? this.id,
+        table: table ?? table,
+        data: data ?? data,
+        createdAt: createdAt ?? this.createdAt,
+        isSynced: isSynced ?? this.isSynced,
+        syncedAt: syncedAt ?? this.syncedAt,
+        syncAction: syncAction ?? this.syncAction,
+      );
+
+  static CachedData fromJson(Map<String, Object?> json) => CachedData(
+        id: json['_id'] as int,
+        table: json['_table'] as String,
+        data: json['_data'] as String,
+        createdAt: json['_createdAt'] as DateTime,
+        isSynced: json['_isSynced'] as bool,
+        syncedAt: json['_syncedAt'] as DateTime,
+        syncAction: json['_syncAction'] as String,
+      );
+
+  Map<String, Object?> toJson() => {
+        CachedDataFields.id: id,
+        CachedDataFields.table: table,
+        CachedDataFields.data: data,
+        CachedDataFields.createdAt: createdAt,
+        CachedDataFields.isSynced: isSynced,
+        CachedDataFields.syncedAt: syncedAt,
+        CachedDataFields.syncAction: syncAction,
+      };
+}
